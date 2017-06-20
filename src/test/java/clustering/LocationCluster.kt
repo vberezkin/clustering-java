c95f1fca8f618b839fa4e31e346ba70cbec0d326
Vladimir Berezkin <berezkin@mail.ru>
2017-06-20 18:12:13 +0300

    Correct LocationCluster test

--- src/test/java/clustering/LocationCluster.kt
+++ src/test/java/clustering/LocationCluster.kt
@@ -14,20 +14,40 @@ private val LinkageMaxDistanceMeters = 130.0
 class LocationCluster {
     @Test
     fun test1() {
-        val latitudes = doubleArrayOf(0.0, 1.0, 2.0)
-        val longitudes = doubleArrayOf(0.0, 1.0, 2.0)
-        val normLongitudes = Array(longitudes.size) { i ->
-            val latCos = Math.cos(Math.toRadians(latitudes[i]))
-            longitudes[i] * latCos
+        Assert.assertTrue(clusterCount(
+                latitudes = doubleArrayOf(35.367949, 35.367026),
+                longitudes = doubleArrayOf(24.505020, 24.503913
+                )) == 2)
+    }
+
+    @Test
+    fun test2() {
+        Assert.assertTrue(clusterCount(
+                latitudes = doubleArrayOf(35.367057, 35.367070, 35.367949),
+                longitudes = doubleArrayOf(24.503904, 24.505031, 24.505015
+                )) == 1)
+    }
+
+    @Test
+    fun test3() {
+        Assert.assertTrue(clusterCount(
+                latitudes = doubleArrayOf(35.367057, 35.367057, 35.368409),
+                longitudes = doubleArrayOf(24.503899, 24.505567, 24.505556
+                )) == 3)
+    }
+
+    private fun clusterCount(latitudes: DoubleArray, longitudes: DoubleArray): Int {
+        val latCosines = DoubleArray(latitudes.size) { i ->
+            Math.cos(Math.toRadians(latitudes[i]))
         }
         val degreeStep = EarthRadius * Math.PI / 180
         val linkageDistanceDegree = LinkageMaxDistanceMeters / degreeStep
         val linkageDistanceDegree2 = linkageDistanceDegree * linkageDistanceDegree
         val pointClusters = SimpleClustering.apply(latitudes.size) { first, second ->
-            val dx = normLongitudes[first] - normLongitudes[second]
+            val dx = (longitudes[first] - longitudes[second]) * (latCosines[first] + latCosines[second]) / 2.0
             val dy = latitudes[first] - latitudes[second]
             dx * dx + dy * dy < linkageDistanceDegree2
         }
-        Assert.assertTrue(pointClusters.size == 3)
+        return pointClusters.size
     }
 }
